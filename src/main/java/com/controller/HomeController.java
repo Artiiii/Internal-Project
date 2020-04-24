b0d1c40d4a29b4e4d69b9162ce149061fe002c72
arti <arti04294@gmail.com>
2020-04-24 17:30:25 +0530

    commited spring mvc

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author Arti Pal
+ *
+ */
+package com.controller;
+
+
+
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+@Controller  
+public class HomeController {  
+@RequestMapping("/")  
+  public String display()  
+  {  
+      return "index";  
+  }     
+}  
