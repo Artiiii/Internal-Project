8f37fa9f1443c72b350b249657f22a22e5c92ac7
arti <arti04294@gmail.com>
2020-04-24 18:28:45 +0530

    Addedd Deployments

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;  
 @Controller  
 public class HomeController {  
-@RequestMapping("/")  
+@RequestMapping("/index")  
   public String display()  
   {  
       return "index";  
